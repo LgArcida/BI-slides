dedb7cb301083f0e33837b8cbe06443ccec538ed
Lucas Bandin <lucas.bandin.arcida@gmail.com>
2020-06-11 10:04:38 -0400

    Node app

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require('express');
-var path = require('path');
+const path = require('path');
 const app = express();
 const port = 3000;
-var fs = require('fs');
-var files = fs.readdirSync(__dirname + '/imgs/');
+const fs = require('fs');
+const files = fs.readdirSync(__dirname + '/imgs/');
 
 app.use(express.static(path.join(__dirname, 'imgs')));
 app.engine('html', require('ejs').renderFile);
 app.get('/', function (request, response) {
     response.render(__dirname + '/index.html', {files: files});
-
 });
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(process.env.PORT || 8080);
