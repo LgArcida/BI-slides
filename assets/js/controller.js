71263f37436de6e74900463ee11bf98a59fa8d96
Lucas Bandin <lucas.bandin.arcida@gmail.com>
2020-07-29 17:51:36 -0400

    New logic

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -126,4 +126,24 @@ function test() {
             });
         }
     });
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", dir, true);
+    xhr.responseType = 'document';
+    xhr.onload = () => {
+        if (xhr.status === 200) {
+            var elements = xhr.response.getElementsByTagName("a");
+            for (x of elements) {
+                if (x.href.match(/\.(jpe?g|png|jpg)$/)) {
+                    let img = document.createElement("img");
+                    img.src = x.href;
+                    document.body.appendChild(img);
+                }
+            }
+            ;
+        } else {
+            alert('Request failed. Returned status of ' + xhr.status);
+        }
+    }
+    xhr.send()
 }
